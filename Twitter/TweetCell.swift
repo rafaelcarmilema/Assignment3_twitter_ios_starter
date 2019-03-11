2c78f48eda78bc0454a24a0f18547c93971e7e52
Rafael Carmilema <rafaelcarmilema@gmail.com>
2019-03-10 20:51:13 -0400

    added circular images and fixed image for being to blurry

--- Twitter/TweetCell.swift
+++ Twitter/TweetCell.swift
@@ -17,9 +17,17 @@ class TweetCell: UITableViewCell {
     @IBOutlet weak var tweetContentLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
+        makeCircularImage()
         // Initialization code
     }
 
+    func makeCircularImage(){
+        profileImageView.layer.borderWidth = 1
+        profileImageView.layer.masksToBounds = false
+        profileImageView.layer.borderColor = UIColor.black.cgColor
+        profileImageView.layer.cornerRadius = profileImageView.frame.height/2
+        profileImageView.clipsToBounds = true
+    }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
